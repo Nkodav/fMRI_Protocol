273c74cd41af240a6519dbaffa14638e6bfd1e56
Dalton <tdaltoncombs@gmail.com>
2014-02-28 21:13:00 -0800

    The fprint is still a bit odd

--- GARPSubjectSingleRun.m
+++ GARPSubjectSingleRun.m
@@ -74,7 +74,6 @@ while key ~= '5'
     end
 end 
 UT = GetSecs;
-sprintf('\nScanner Triggered at %3.3f s\n',StartSecs);
 %%
 control1 = 2*(ones(1,50));  % later referred to as "case 2"
 control2 = 3*(ones(1,40));  % later referred to as "case 3"
@@ -188,6 +187,14 @@ settings.items.item15 = items{15}; settings.items.item16 = items{16};
 settings.items.item17 = items{17}; settings.items.item18 = items{18};
 settings.items.item19 = items{19}; settings.items.item20 = items{20};
 settings.items.item21 = items{21};
+settings.controlConditionOrder1 = controlConditionOrder1;
+settings.controlConditionOrder2 = controlConditionOrder2;
+settings.scalingConditionOrder2 = scalingConditionOrder2;
+settings.scalingConditionOrder3 = scalingConditionOrder3;
+settings.scalingConditionOrder4 = scalingConditionOrder4;
+settings.bundlingConditionOrder2 = bundlingConditionOrder2;
+settings.bundlingConditionOrder3 = bundlingConditionOrder3;
+settings.bundlingConditionOrder4 = bundlingConditionOrder4;
 settings.controlSet = Ncontrol1Set;
 settings.control2Set = Ncontrol2Set;
 settings.scaling2Set = Nscaling2Set;
@@ -196,6 +203,7 @@ settings.scaling4Set = Nscaling4Set;
 settings.bundling2Set = Nbundling2Set;
 settings.bundling3Set = Nbundling3Set; 
 settings.bundling4Set = Nbundling4Set;
+settings.trialOrder   = trialOrder;
 settings.screenNumber = screenNumber;
 settings.width = width;
 settings.height = height;
@@ -205,8 +213,10 @@ settings.height = height;
 mkdir(settings.recordfolder);
 % create the file name for this run of this subject
 recordname = [settings.recordfolder '/' num2str(subjID) '_' num2str(j) '_' datestr(now,'yyyymmddTHHMMSS') '.mat'];
+textFileName = [settings.recordfolder '/' num2str(subjID) '_' num2str(j) '_' datestr(now,'yyyymmddTHHMMSS') '.txt'];
 % Save the settings (the results are saved later)
 save (recordname, 'settings')
+fileID = fopen(textFileName, 'w');
 
 
 %%
@@ -232,14 +242,18 @@ end
 
 % whenTime(length(time)+1,1) = UT + j*10 + 324 + time(k);
 
-RestrictKeysForKbCheck([30, 32, 32, 33]); % these are the keycodes for 1,2,3,4 on a Mac
+RestrictKeysForKbCheck([30, 31, 32, 33]); % these are the keycodes for 1,2,3,4 on a Mac
 
 while i <= long;
     switch trialOrder(i);
         case 1 %is for the NULL condition
+            caseNumber = 1;
+            setNumber = 0;
             drawFixation(w);
             
         case 2 %is for the CONTROL condition (1st half: 5 reps)
+            caseNumber = 2;
+            setNumber = controlConditionOrder1(controlIndex1);
             itemCode = Ncontrol1Set(controlConditionOrder1(controlIndex1));
             v1 = items{itemCode};
             v2 = items{itemCode};
@@ -253,6 +267,8 @@ while i <= long;
             
         
         case 3 %is for the CONTROL condition (2nd half: 4 reps)
+            caseNumber = 3;
+            setNumber = controlConditionOrder2(controlIndex2);
             itemCode = Ncontrol2Set(controlConditionOrder2(controlIndex2));
             v1 = items{itemCode};
             v2 = items{itemCode};
@@ -265,6 +281,8 @@ while i <= long;
             controlIndex2 = controlIndex2  + 1;
 
         case 4 %is for the SCALING by 2 condition
+            caseNumber = 4;
+            setNumber = scalingConditionOrder2(scalingIndex2);
             itemCode = Nscaling2Set(scalingConditionOrder2(scalingIndex2),1);
             v1 = items{itemCode};
             v2 = items{itemCode};
@@ -277,6 +295,8 @@ while i <= long;
             scalingIndex2 = scalingIndex2  + 1;
         
         case 5 %is for the SCALING by 3 condition
+            caseNumber = 5;
+            setNumber = scalingConditionOrder3(scalingIndex3);
             itemCode = Nscaling3Set(scalingConditionOrder3(scalingIndex3),1);
             v1 = items{itemCode};
             v2 = items{itemCode};
@@ -289,6 +309,8 @@ while i <= long;
             scalingIndex3 = scalingIndex3 + 1;
         
         case 6 %is for the SCALING by 4 condition
+            caseNumber = 6;
+            setNumber = scalingConditionOrder4(scalingIndex4);
             itemCode = Nscaling4Set(scalingConditionOrder4(scalingIndex4),1);
             v1 = items{itemCode};
             v2 = items{itemCode};
@@ -301,6 +323,8 @@ while i <= long;
             scalingIndex4 = scalingIndex4  + 1;
         
         case 7 %is for the BUNDLING by 2 condition
+            caseNumber = 7;
+            setNumber = bundlingConditionOrder2(bundlingIndex2);
             itemCode1 = Nbundling2Set(bundlingConditionOrder2(bundlingIndex2),1);
             itemCode2 = Nbundling2Set(bundlingConditionOrder2(bundlingIndex2),2);
             v1 = items{itemCode1};
@@ -314,6 +338,8 @@ while i <= long;
             bundlingIndex2 = bundlingIndex2 + 1;
         
         case 8 %is for the BUNDLING by 3 condition
+            caseNumber = 8;
+            setNumber = bundlingConditionOrder3(bundlingIndex3);
             itemCode1 = Nbundling3Set(bundlingConditionOrder3(bundlingIndex3),1);
             itemCode2 = Nbundling3Set(bundlingConditionOrder3(bundlingIndex3),2);
             itemCode3 = Nbundling3Set(bundlingConditionOrder3(bundlingIndex3),3);
@@ -328,6 +354,8 @@ while i <= long;
             bundlingIndex3 = bundlingIndex3 + 1;
         
         case 9 %is for the BUNDLING by 4 condition
+            caseNumber = 9;
+            setNumber = bundlingConditionOrder4(bundlingIndex4);
             itemCode1 = Nbundling4Set(bundlingConditionOrder4(bundlingIndex4),1);
             itemCode2 = Nbundling4Set(bundlingConditionOrder4(bundlingIndex4),2);
             itemCode3 = Nbundling4Set(bundlingConditionOrder4(bundlingIndex4),3);
@@ -342,7 +370,7 @@ while i <= long;
             settings.cueLocation(i,1:2) = s;
             bundlingIndex4 = bundlingIndex4 + 1;
     end
-    
+    fprintf(fileID,'%d\t%d\t','caseNumber','setNumber');
 
     [VBLTimestamp, StimulusOnsetTime, FlipTimestamp] = Screen('Flip', w, whenTime(i,1));
     settings.VBLTimestamp(i) = VBLTimestamp;
@@ -362,39 +390,36 @@ while i <= long;
             behavioral.choice(i,1) = 'r';
             feedbackLogic('1',numberItems,r,s,v1,v2,v3,v4,w);
             Screen('Flip',w);
-            WaitSecs(feedbackTime);
             drawFixation(w);
-            Screen('Flip',w);
+            Screen('Flip',w,behavioral.secs(i)+feedbackTime);
         elseif (strcmp(KbName(keyCode),'3#') || strcmp(KbName(keyCode),'4$')) && (s(1)==1);
             behavioral.key(i,1) = '3';
             behavioral.choice(i,1) = 'v';
-            feedbackLogic('3',numberItems,r,s,v1,v2,v3,v4,w);
+            feedbackLogic('3',numberItems,r,s,v1,v2,v3,v4,w);            
             Screen('Flip',w);
-            WaitSecs(feedbackTime);
             drawFixation(w);
-            Screen('Flip',w);
+            Screen('Flip',w,behavioral.secs(i)+feedbackTime);
         elseif (strcmp(KbName(keyCode),'1!') || strcmp(KbName(keyCode),'2@')) && (s(1)==2);
             behavioral.key(i,1) = '1';
             behavioral.choice(i,1) = 'v';
             feedbackLogic('1',numberItems,r,s,v1,v2,v3,v4,w);
             Screen('Flip',w);
-            WaitSecs(feedbackTime);
             drawFixation(w);
-            Screen('Flip',w);
+            Screen('Flip',w,behavioral.secs(i)+feedbackTime);
         elseif (strcmp(KbName(keyCode),'3#') || strcmp(KbName(keyCode),'4$')) && (s(1)==2);
             behavioral.key(i,1) = '3';
             behavioral.choice(i,1) = 'r';
             feedbackLogic('3',numberItems,r,s,v1,v2,v3,v4,w);
             Screen('Flip',w);
-            WaitSecs(feedbackTime);
             drawFixation(w);
-            Screen('Flip',w);
+            Screen('Flip',w,behavioral.secs(i)+feedbackTime);
         else
             drawFixation(w);
             Screen('Flip',w);
             behavioral.key(i,1) = '0';
             behavioral.choice(i,1) = 'n';
         end
+        fprintf(fileID,'%f\t%f\n','StimulusOnsetTime','behavioral.secs(i)');
     end
     
     
@@ -406,7 +431,8 @@ end
 %% at the end
 drawStop(w);
 Screen('Flip',w);
-save (recordname, 'behavioral', '-append')
-save(recordname,'settings')
+save (recordname, 'settings');
+save (recordname, 'behavioral', '-append');
+fclose(fileID);
 Screen('CloseAll');
 end
